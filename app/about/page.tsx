55a91d68f482232a509c2a672a783df15170a735
dogsteven <caigicungduoc23@gmail.com>
2024-04-05 19:13:23 +0700

    update about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,29 +9,44 @@ export default async function Home() {
         <div className="grid grid-cols-2 w-full gap-4">
           <div>
             <p>
-              This system consists 4 components: the SFUs, the Broker, the Socket Handler and the Web Application.
+              This system includes four components: the SFUs, the Broker, the Socket Handler, and the Web Application.
             </p>            
             <ol className="list-decimal list-inside">
               <li>The <b>SFUs</b> (Selective Forwarding Units) manage the logic of meetings and their attendees in the media plane.</li>
-              <li>The <b>Broker</b> works similarly to a reversed proxy: it selectively picks an SFU for instantiating a meeting and forwards each command to an appropriate SFU.</li>
+              <li>The <b>Broker</b> works similarly to a reversed proxy: it selectively picks an SFU for instantiating a meeting and forwards each command to the appropriate SFU.</li>
               <li>The <b>Socket Handler</b> handles the connections of attendees in each meeting. It also acts as an instant messaging service that notifies attendees about events that have taken place.</li>
-              <li>The <b>Web Application</b> is an interactive web application that users use to start and join a video conference.</li>
+              <li>The <b>Web Application</b> is an interactive web application that users use to start and join video conferences.</li>
             </ol>
             <br />
 
             <p>
-              At this moment, we choose <a href="https://mediasoup.org/" className="underline">mediasoup</a> to implement the SFUs for its performance, signaling agnostic and working seamlessly on many operating systems.
+              Currently, we choose <a href="https://mediasoup.org/" className="underline">mediasoup</a> to implement the SFUs because of its performance, signaling agnostic, scalability, and working seamlessly on many operating systems.
             </p>
             <br />
 
             <p>
-              The Broker maintains an in-memory hash map storing information about each meeting and its corresponding SFU&apos;s address. Of course, we can move that hash map to persistent storage to make the Broker stateless and scale out to multiple nodes to produce higher availability. We keep the map in memory for simplicity. The round-robin algorithm is applied to select an SFU for a new meeting.
+              The Broker maintains an in-memory hash map storing information about each meeting and its corresponding SFU&apos;s address. For simplicity, we adopt the round-robin algorithm to select an SFU for a new meeting.
             </p>
             <br />
 
             <p>
-              Most of the communication methods used in the system are synchronous, but there are some cases when the SFUs want to notify attendees to perform appropriate actions on local machines.
-              To decouple the SFUs and the Socket Handler, <a href="https://redis.io/" className="underline">Redis</a> is used as the communication channel because of its simplicity and low latency.
+              We use <a href="https://nextjs.org/" className="underline">Next.js</a> for the Web Application and <a href="https://socket.io/" className="underline">socket.io</a> for the Socket Handler. Because the application logic other than the media itself is relatively small, we decided to put all the application logic into the Socket Handler.
+              For complex scenarios, we suggest separating the application logic into another server.
+            </p>
+            <br />
+
+            <p>
+              Most of the communication methods used in the system are synchronous. There are still some cases when the SFUs want to notify attendees to perform appropriate actions on local machines, which requires a communication channel between the SFUs and the Socket Handler. We decided to utilize <a href="https://redis.io/" className="underline">Redis</a> because of its simplicity, reliability, and low latency.
+            </p>
+            <br />
+
+            <p>
+              The system can be horizontally scalable in the following ways:
+              <ol className="list-decimal list-inside">
+                <li>Move the in-memory map to a remote storage service to make the Broker stateless.</li>
+                <li>Adapt Redis backplane to the Socket Handler.</li>
+                <li>The Next.js Web Application is naturally horizontally scalable.</li>
+              </ol>
             </p>
           </div>
           <div>
